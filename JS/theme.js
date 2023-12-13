816b8fd349f0d19b134f31b07d57046925d49711
joaoguilherme1603 <martinslino1603gt@gmail.com>
2023-12-13 10:49:49 -0300

    up

--- JS/theme.js
+++ JS/theme.js
@@ -1,4 +1,5 @@
 function darkMode() {
+    // Nessas duas linhas de código eu defino uma varíavel e quando a funcão em questão é chamada ele adiciona a classe "darkMode" ao elemento body, para dar toggle no modo escuro.
     var element = document.body;
     element.classList.toggle("darkMode")
 }
